f19c8b851756132572f39cfbc1b7136793306c0e
12174819 <dhdudwn51@naver.com>
2024-12-12 16:54:31 +0900

    home_page UI re-union.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -42,7 +42,9 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         final itemList = _itemsPerDate[_selectedDate.toString()] ?? [];
         if (itemList.length < 7) {
-          itemList.add(Item(emoji: selectedItem['emoji'], text: selectedItem['text'], checked: false));
+          itemList.add(Item(emoji: selectedItem['emoji'],
+              text: selectedItem['text'],
+              checked: false));
           _itemsPerDate[_selectedDate.toString()] = itemList;
         } else {
           ScaffoldMessenger.of(context).showSnackBar(
@@ -56,8 +58,10 @@ class _HomePageState extends State<HomePage> {
   List<DateTime> _getCurrentWeekDates() {
     DateTime today = _selectedDate;
     int currentWeekday = today.weekday;
-    DateTime firstDayOfWeek = today.subtract(Duration(days: currentWeekday - 1));
-    return List.generate(7, (index) => firstDayOfWeek.add(Duration(days: index)));
+    DateTime firstDayOfWeek = today.subtract(
+        Duration(days: currentWeekday - 1));
+    return List.generate(
+        7, (index) => firstDayOfWeek.add(Duration(days: index)));
   }
 
   void _toggleCheck(Item item) {
@@ -83,7 +87,8 @@ class _HomePageState extends State<HomePage> {
                 padding: const EdgeInsets.all(16),
                 child: Text(
                   currentMonthYear,
-                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ),
               GestureDetector(
@@ -91,11 +96,13 @@ class _HomePageState extends State<HomePage> {
                   if (details.primaryVelocity != null) {
                     if (details.primaryVelocity! < 0) {
                       setState(() {
-                        _selectedDate = _selectedDate.add(const Duration(days: 7));
+                        _selectedDate = _selectedDate.add(const Duration(
+                            days: 7));
                       });
                     } else if (details.primaryVelocity! > 0) {
                       setState(() {
-                        _selectedDate = _selectedDate.subtract(const Duration(days: 7));
+                        _selectedDate = _selectedDate.subtract(const Duration(
+                            days: 7));
                       });
                     }
                   }
@@ -116,23 +123,27 @@ class _HomePageState extends State<HomePage> {
                         },
                         child: Container(
                           decoration: BoxDecoration(
-                            color: isSelected ? Colors.green : Colors.transparent,
+                            color: isSelected ? Colors.green : Colors
+                                .transparent,
                             borderRadius: BorderRadius.circular(8),
                           ),
                           padding: const EdgeInsets.all(8),
                           child: Column(
                             children: [
                               Text(
-                                ['월', '화', '수', '목', '금', '토', '일'][date.weekday - 1],
+                                ['월', '화', '수', '목', '금', '토', '일'][date
+                                    .weekday - 1],
                                 style: TextStyle(
-                                  color: isSelected ? Colors.white : Colors.black,
+                                  color: isSelected ? Colors.white : Colors
+                                      .black,
                                 ),
                               ),
                               const SizedBox(height: 4),
                               Text(
                                 '${date.day}',
                                 style: TextStyle(
-                                  color: isSelected ? Colors.white : Colors.black,
+                                  color: isSelected ? Colors.white : Colors
+                                      .black,
                                   fontWeight: FontWeight.bold,
                                 ),
                               ),
@@ -145,7 +156,44 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               const SizedBox(height: 20),
-              Expanded(child: _buildTable(currentItems)),
+              Expanded(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding:
+                      const EdgeInsets.symmetric(horizontal: 30), // 회색 박스
+                      child: Container(
+                        width: double.infinity,
+                        color: Colors.grey.shade200,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '☀️', // 해 이모지
+                              style: TextStyle(
+                                fontSize: 40, // 이모지 크기
+                              ),
+                            ),
+                            const SizedBox(width: 8), // 이모지와 텍스트 사이 간격
+                            Text(
+                              '오늘의 루틴', // 텍스트
+                              style: TextStyle(
+                                fontSize: 30, // 텍스트 크기
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green, // 텍스트 색상
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(child: _buildTable(currentItems)),
+                  ],
+                ),
+              ),
             ],
           ),
           Positioned(
@@ -172,20 +220,26 @@ class _HomePageState extends State<HomePage> {
           TableRow(
             children: [
               Container(
-                height: 60,
-                alignment: Alignment.center,
-                child: Text(item.emoji, style: const TextStyle(fontSize: 32)),
+                  height: 50, // 높이 지정
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(item.emoji, style: const TextStyle(fontSize: 25),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        item.text,
+                        style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green),
+                      ),
+                    ],
+                  )
               ),
               Container(
-                height: 60,
-                alignment: Alignment.center,
-                child: Text(
-                  item.text,
-                  style: const TextStyle(fontSize: 18),
-                ),
-              ),
-              Container(
-                height: 60,
+                height: 50,
                 alignment: Alignment.center,
                 child: Checkbox(
                   value: item.checked,
@@ -195,7 +249,7 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               Container(
-                height: 60,
+                height: 50,
                 alignment: Alignment.center,
                 child: IconButton(
                   icon: const Icon(Icons.delete, color: Colors.red),
@@ -217,22 +271,17 @@ class _HomePageState extends State<HomePage> {
           TableRow(
             children: [
               Container(
-                height: 60,
+                height: 50,
                 alignment: Alignment.center,
                 child: const SizedBox(),
               ),
               Container(
-                height: 60,
+                height: 50,
                 alignment: Alignment.center,
                 child: const SizedBox(),
               ),
               Container(
-                height: 60,
-                alignment: Alignment.center,
-                child: const SizedBox(),
-              ),
-              Container(
-                height: 60,
+                height: 50,
                 alignment: Alignment.center,
                 child: const SizedBox(),
               ),
@@ -242,24 +291,49 @@ class _HomePageState extends State<HomePage> {
       }
     }
 
-    return Container(
-      padding: const EdgeInsets.all(16),
-      color: Colors.grey[200],
-      child: Table(
-        border: TableBorder.all(
-          color: Colors.grey,
-          width: 2,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        columnWidths: const {
-          0: FixedColumnWidth(60),
-          1: FlexColumnWidth(),
-          2: FixedColumnWidth(60),
-          3: FixedColumnWidth(60),
-        },
-        children: tableRows,
-      ),
-    );
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: List.generate(tableRows.length, (index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.5),
+                child: Text(
+                  '${index + 1}',
+                  style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey),),
+              );
+            }),
+          ),
+          const SizedBox(width: 15),
+          Expanded(
+            child: Table(
+              border: TableBorder.all(
+                color: Colors.grey,
+                width: 2,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              columnWidths: const {
+                0: FlexColumnWidth(),
+                1: FixedColumnWidth(50),
+                2: FixedColumnWidth(50),
+              },
+              children: tableRows.map((row) {
+                return TableRow(
+                  children: row.children.map((cell) {
+                    return Container(
+                      color: Colors.grey.shade200, // 셀 내부 배경색을 회색으로 설정
+                      child: cell,
+                    );
+                  }).toList(),
+                );
+              }).toList(),
+            ),
+          )
+        ]));
   }
 }
 
@@ -269,9 +343,8 @@ class Item {
   bool checked;
 
   Item({
-    required this.emoji,
-    required this.text,
-    required this.checked,
+  required this.emoji,
+  required this.text,
+  required this.checked,
   });
 }
-
